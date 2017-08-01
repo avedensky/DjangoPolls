a3f6b2daccda98eb2b25912e0e3b5d25161cbb09
Alexey Vedensky <avedensky@gmail.com>
2017-08-01 18:25:57 +0300

    Add:lesson 4

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ha@cf3t199w8^*26p2gnsvo8d@ojdokrp0-#z4gsyu%!=f@4^2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 ]
 
 MIDDLEWARE = [
